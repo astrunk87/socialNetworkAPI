85fc4a4164c58ff72fc72b04f5a702b6796a84e3
astrunk87 <134554632+astrunk87@users.noreply.github.com>
2023-10-20 12:25:23 -0500

    started work on models and added packages

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const { Schema, Types} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+            // regex pulled from regex hw
+        },
+        thoughts: [thoughtsSchema],
+        friends: {
+
+        }
+
+    }
+)
+
+const user = model('user', userSchema);
+
+module.exports = user;
